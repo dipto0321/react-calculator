f0145e1939bd7696ddc4fb9040d08864c11fda33
Dipto Karmakar <diptokmk47@gmail.com>
2019-04-15 15:30:25 +0600

    Remove unnecessary code from Display.js

--- src/js/components/Display.js
+++ src/js/components/Display.js
@@ -3,12 +3,7 @@ import PropTypes from 'prop-types';
 
 const Display = (props) => {
   const { total, next, error } = props.displayArgs;
-  return (
-    <div className="display">
-      {error === null ? (next === null ? total : next) : error}
-      {' '}
-    </div>
-  );
+  return <div className="display">{error === null ? (next === null ? total : next) : error}</div>;
 };
 
 Display.propTypes = {
